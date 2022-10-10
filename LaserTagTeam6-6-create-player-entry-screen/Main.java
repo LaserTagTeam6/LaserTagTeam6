c6114790a6f3587dc3cbc08c1d9340ca353392c5
thwinkle01 <112516821+thwinkle01@users.noreply.github.com>
2022-10-09 22:11:53 -0500

    Add files via upload

---
+++ LaserTagTeam6-6-create-player-entry-screen/Main.java
@@ -0,0 +1,48 @@
+/*
+Name: Team 6
+Project: Laser Tag
+Date: 09/30/2022
+*/
+
+import javax.swing.JFrame;
+import java.awt.Toolkit;
+
+public class Main extends JFrame
+{
+	Model model;
+	View view;
+	Controller controller;
+	
+	public Main()
+	{
+		model = new Model();
+		view = new View(model);
+		controller = new Controller(model, view);
+		view.show(view);
+		view.addMouseListener(controller);
+		view.addKeyListener(controller);
+	}
+	
+	public void run()
+	{
+		while(true)
+		{
+			controller.update();
+			model.update();
+			//view.repaint();
+			Toolkit.getDefaultToolkit().sync();
+			try{
+				Thread.sleep(1);
+			} catch(Exception e){
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		Main g = new Main();
+		g.run();
+	}
+}
